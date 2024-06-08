9d08cbaea367027097bbb2ba0e019f82f601349b
Arpita Dwivedi <arpitad.ug22.cs@nitp.ac.in>
2024-06-09 00:58:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 // import logo from './logo.svg';
 import "./App.css";
 
-import Header from "./UI/components/Header";
-import Main from "./UI/components/Main";
-
 function App() { 
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      {/* <p>
+      <header className="App-header"> */}
+      <img src={logo} className="App-logo" alt="logo" /> 
+      <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-      {/* <a
+        </p>
+      <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
-      {/* </header> */}
-      <Header/>
-      <Main/>
+        </a> 
+      </header>
+      
     </div>
   );
 }
